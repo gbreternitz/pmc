b36319dc1b690783596e496f68ea0dbe060c8dd8
gbreternitz <gabrielbreternitz@gmail.com>
2024-02-05 21:46:10 -0600

    built project structure for java backend

--- src/main/java/project/pmc/models/AbstractEntity.java
+++ src/main/java/project/pmc/models/AbstractEntity.java
@@ -1,5 +1,34 @@
 package project.pmc.models;
 
-public class AbstractEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
